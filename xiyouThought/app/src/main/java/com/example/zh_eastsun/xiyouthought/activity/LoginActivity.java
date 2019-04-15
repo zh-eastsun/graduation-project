f7c193865f4e0172eb289b9467fd1adfa522f5f4
zh_eastsun <zh_eastsun@163.com>
2019-04-15 22:32:22 +0800

    清除重构前使用线程池和handler部分的代码块

--- xiyouThought/app/src/main/java/com/example/zh_eastsun/xiyouthought/activity/LoginActivity.java
+++ xiyouThought/app/src/main/java/com/example/zh_eastsun/xiyouthought/activity/LoginActivity.java
@@ -14,8 +14,6 @@ import com.example.zh_eastsun.xiyouthought.R;
 import com.example.zh_eastsun.xiyouthought.net.NetUtil;
 import com.example.zh_eastsun.xiyouthought.rxjava.VerifyManager;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,11 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText inputPassword;
     private Button login;
     private VerifyManager verifyManager;
-    //执行耗时任务的线程池，最大工作线程数为5
-    //private ExecutorService executorService = Executors.newFixedThreadPool(5);
     private Handler handler = new Handler(Looper.getMainLooper());
-
-
 
 
     //完成控件的初始化
@@ -35,7 +29,7 @@ public class LoginActivity extends AppCompatActivity {
         inputStuNum = findViewById(R.id.input_stuNum);
         inputPassword = findViewById(R.id.input_password);
         login = findViewById(R.id.login);
-        verifyManager = new VerifyManager(LoginActivity.this,inputStuNum,inputPassword);
+        verifyManager = new VerifyManager(LoginActivity.this, inputStuNum, inputPassword);
         verifyManager.setNetRequestCallback(new VerifyManager.NetRequestCallback() {
             @Override
             public void success() {
@@ -47,54 +41,6 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (NetUtil.isNetPingUsable()) {
                     verifyManager.loginVerify();
-//                    //等待提示框
-//                    final AlertDialog dialog = new AlertDialog.Builder(LoginActivity.this)
-//                            .setTitle("正在登录")
-//                            .setMessage("请等待...")
-//                            .setView(R.layout.wait_progress_bar)
-//                            .setCancelable(false)
-//                            .show();
-//                    //获取用户输入的学号、密码和验证码信息
-//                    final String stuNum = inputStuNum.getText().toString();
-//                    final String password = inputPassword.getText().toString();
-//                    //初始化发起登陆请求的类
-//                    final XUPTVerify verify = new XUPTVerify();
-//                    executorService.execute(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            try {
-//                                //判断登录状态是否成功
-//                                //成功时携带学生的基本信息跳转活动
-//                                //失败时清空输入栏
-//                                if (verify.verify(stuNum, password)) {
-//                                    handler.post(new Runnable() {
-//                                        @Override
-//                                        public void run() {
-//                                            dialog.dismiss();
-//                                            Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT)
-//                                                    .show();
-//                                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                                            startActivity(intent);
-//                                            finish();
-//                                        }
-//                                    });
-//                                } else {
-//                                    handler.post(new Runnable() {
-//                                        @Override
-//                                        public void run() {
-//                                            dialog.dismiss();
-//                                            Toast.makeText(LoginActivity.this, "账号密码错误，请重新登录", Toast.LENGTH_SHORT)
-//                                                    .show();
-//                                            inputStuNum.setText("");
-//                                            inputPassword.setText("");
-//                                        }
-//                                    });
-//                                }
-//                            } catch (Exception e) {
-//                                e.printStackTrace();
-//                            }
-//                        }
-//                    });
                 } else {
                     Toast.makeText(LoginActivity.this, R.string.internet_unavailable_hint, Toast.LENGTH_SHORT)
                             .show();
